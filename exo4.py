ad4ac1ba8d067c66f5d69eedd8b93472406f9b8a
idrisBellahsen <39699663+idrisBellahsen@users.noreply.github.com>
2019-12-04 14:36:10 +0100

    Exercice 4 + bonus sauf bonus 3

--- exo4.py
+++ exo4.py
@@ -1,13 +1,23 @@
 def cascadeDouble(list):
     # Début de ton code
-    pass
+    
+    if isinstance(list,int):    
+        return list*2
+    
+    list2= []
+    for number in list :
+        if isinstance(number, int):
+            list2.append(number*2)
+        else:
+            raise Exception("Ce n'est pas un nombre")
+    return list2    
     # Fin de ton code
 
 
 
 # Pas touche!
 tests = (
-    ([1, 2, 3], [4, 5, 6]),
+    ([1, 2, 3], [2, 4, 6]),
     ([10], [20]),
     ([1, 1, 1, 1, 1], [2, 2, 2, 2, 2]),
     ([-12, 0, 50], [-24, 0, 100]),
@@ -15,3 +25,6 @@ tests = (
 
 for test in tests:
     print(f"L'appel  cascadeDouble({test[0]})  renvoie: {cascadeDouble(test[0])} (résultat attendu: {test[1]})")
+
+
+
